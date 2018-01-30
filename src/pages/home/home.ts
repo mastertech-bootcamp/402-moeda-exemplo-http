c46a93f73dcd60c9c1f4569c337af4fe142963ff
Bruno Pimentel <brunoapimentel@gmail.com>
2018-01-30 16:13:57 -0200

    Busca de dados da API do Fixer

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,23 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  dados;
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, public http: HttpClient) {
+  }
 
+  buscarDados(){
+    this.http
+      .get('https://api.fixer.io/latest')
+      .subscribe((resposta) => {
+        this.dados = resposta;
+      });
   }
 
 }
